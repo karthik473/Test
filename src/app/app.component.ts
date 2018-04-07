6facd841ea724812831dc99461a21a58961ffb8f
karthik473 <karthikmperumalla@gmail.com>
2018-04-07 07:31:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { AmazingTimePickerService } from 'amazing-time-picker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ 
+
+  public selectedTime: string;
+
+    constructor( private atp: AmazingTimePickerService ) { }
+
+    open() {
+        const amazingTimePicker = this.atp.open();
+        amazingTimePicker.afterClose().subscribe(time => {
+            this.selectedTime = time;
+            console.log(this.selectedTime);
+        });
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AmazingTimePickerModule } from 'amazing-time-picker';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmazingTimePickerModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
